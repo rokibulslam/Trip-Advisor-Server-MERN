40003222d9975e37d7eec2272509c2b4c3669421
rokibulslam <rokibulslam6@gmail.com>
2021-10-31 19:04:12 +0600

    prepearing for deploy

--- index.js
+++ index.js
@@ -92,6 +92,7 @@ async function run() {
                 })
             res.send(result)
         })
+        // Delete 
         app.delete('/deleteService/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) }
